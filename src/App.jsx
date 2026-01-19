fc5216f0a3df494f2d83d08c00e7a41c35d8f750
SURAJ KUMAR SHARMA <75290099+sharma62@users.noreply.github.com>
2026-01-19 07:48:24 +0000

    session - 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import CartPage from "./pages/CartPage"
 
 function App() {
+  function Navbar() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand fw-bold" to="Home.html">🛒 MyShop</Link>
+          <div>
+            <Link className="nav-link d-inline me-3 text-white" to="Home.html">Home</Link>
+            <Link className="nav-link d-inline text-white" to="cart.html">Cart (2)</Link>
+          </div>
+        </div>
+      </nav>
+    )
+  }
 
   return (
-    <>
-      shopping cart
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={ <Home/>} />
+        <Route path="/cart" element={ <CartPage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
