e3f680eb0f6f4cfe5098a60f8659ec860e0b4ee9
SURAJ KUMAR SHARMA <75290099+sharma62@users.noreply.github.com>
2026-01-24 01:21:11 +0000

    Revert "signup ui ready"
    This reverts commit 196eca4c8d24b0cf67d3c9e1b931ff6dc99454f2.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import CartPage from "./pages/CartPage"
 import { useCart } from "./context/CartContext"
-import Signup from "./pages/Signup"
 
 function App() {
   const { cart } = useCart();
@@ -12,8 +11,6 @@ function App() {
         <div className="container">
           <Link className="navbar-brand fw-bold" to="/">🛒 MyShop</Link>
           <div>
-
-            <Link className="nav-link d-inline me-3 text-white" to="/signup">Sign Up</Link>
             <Link className="nav-link d-inline me-3 text-white" to="/home">Home</Link>
             <Link className="nav-link d-inline text-white" to="/cart"> 🛒 Cart ({cart.length})</Link>
           </div>
@@ -28,7 +25,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/signup" element={<Signup/>} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
     </BrowserRouter>
